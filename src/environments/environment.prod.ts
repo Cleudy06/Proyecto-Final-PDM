efdf8b84f852eeea149b314ea5a1209794fca353
Cleudy06 <166656981+Cleudy06@users.noreply.github.com>
2024-04-10 22:57:45 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  // Your web app's Firebase configuration
+    firebaseConfig: {
+      apiKey: "AIzaSyCX4tHDOLY_zChEx4Eu5pApGO7QSF7WBa0",
+      authDomain: "product-admin-app-76a90.firebaseapp.com",
+      projectId: "product-admin-app-76a90",
+      storageBucket: "product-admin-app-76a90.appspot.com",
+      messagingSenderId: "224810101547",
+      appId: "1:224810101547:web:cda09c8018eb42019c336e"
+    }
+  };
+ 
